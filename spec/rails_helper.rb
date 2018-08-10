571e9d9e4ad34f3e11278d848e0058f590bd8f16
EC2 Default User <ec2-user@ip-172-31-38-93.us-east-2.compute.internal>
2018-08-10 06:32:36 +0000

    cobertura

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,6 @@
 require 'simplecov'
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 SimpleCov.start 'rails'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
